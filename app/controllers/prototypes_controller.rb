c8dbd061ff3c774825e85f11891e04ac84861214
jinyaishida <100757861+jinyaishida@users.noreply.github.com>
2022-03-28 16:55:16 +0900

    課題修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :destroy, :edit]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -44,11 +44,6 @@ class PrototypesController < ApplicationController
     redirect_to root_path
     end
 
-    def move_to_index
-      unless @prototype.user_id == current_user.id
-        redirect_to action: :index
-      end
-    end
 
 
   private
@@ -57,6 +52,13 @@ class PrototypesController < ApplicationController
    params.require(:prototype).permit(:title, :catch_copy, :concept, :user, :image).merge(user_id: current_user.id)
  end
 
+
+ def move_to_index
+  @prototype = Prototype.find(params[:id])
+  unless user_signed_in? && current_user.id == @prototype.user_id
+    redirect_to action: :index
+  end
+end
 
 
 end
